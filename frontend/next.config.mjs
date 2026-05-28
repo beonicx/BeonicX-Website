584fe5223f9b845793ec56ac7be8ff0bac177bf0
NITISH YADAV <101405119+thisisnitishyadav@users.noreply.github.com>
2026-05-29 00:17:19 +0530

    vercel

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,21 @@ const nextConfig = {
   images: {
     domains: ['i.postimg.cc', 'images.pexels.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: process.env.VERCEL_URL?.includes('vercel.app')
+              ? 'noindex, nofollow'
+              : 'index, follow',
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
